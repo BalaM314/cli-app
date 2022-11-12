b95b9d36a69b3d8b3be88806762353873933cfa7
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2022-11-12 14:38:26 +0530

    Don't display unknown argument for "?" or "help"

--- src/Application.ts
+++ src/Application.ts
@@ -195,7 +195,7 @@ Usage: ${this.name} [command] [options]
 			//Loop through each named argument passed
 			Object.keys(parsedArgs.namedArgs).forEach(arg => 
 				//If the arg is not in the named arguments or the aliases
-				(arg in command!.argOptions.namedArgs || arg in (command!.argOptions.aliases ?? {})) ? "" :
+				(arg in command!.argOptions.namedArgs || arg in (command!.argOptions.aliases ?? {}) || arg == "help" || arg == "?") ? "" :
 					//Display a warning
 					console.warn(`Unknown argument ${arg}`)
 			);
