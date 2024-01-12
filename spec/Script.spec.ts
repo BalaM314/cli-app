d1c1c4e8777074254e371c6d68be81c5bfe4a172
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2024-01-12 20:13:53 +0530

    License under LGPL

--- spec/Script.spec.ts
+++ spec/Script.spec.ts
@@ -1,3 +1,13 @@
+/*
+Copyright © <BalaM314>, 2024.
+This file is part of cli-app.
+cli-app is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+cli-app is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public License along with cli-app. If not, see <https://www.gnu.org/licenses/>.
+
+Contains unit tests for Script.
+*/
+
 import * as path from "path";
 import { Script } from "../src/Script.js";
 import { ApplicationError } from "../src/classes.js";
--- src/Application.ts
+++ src/Application.ts
@@ -1,3 +1,13 @@
+/*
+Copyright © <BalaM314>, 2024.
+This file is part of cli-app.
+cli-app is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+cli-app is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+You should have received a copy of the GNU Lesser General Public License along with cli-app. If not, see <https://www.gnu.org/licenses/>.
+
+Contains the code for the Application class, which represents a command-line application.
+*/
+
 import * as path from "path";
 import { ApplicationError, StringBuilder } from "./classes.js";
 import type { Script } from "./Script.js";
