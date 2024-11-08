221d345c2bc2d2642b66671e5ea461a95b287464
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2024-11-08 19:10:50 +0530

    More TDD

---
+++ spec/src/test-utils.ts
@@ -0,0 +1,41 @@
+
+import path from "node:path";
+import { ApplicationError } from "../../build/classes.js";
+import { ApplicationRunOptions } from "../../src/types.js";
+import { Application } from "../../src/Application.js";
+
+process.chdir(path.join(import.meta.dirname, ".."));
+
+export function applicationError(message:string):jasmine.AsymmetricMatcher<unknown> {
+	return {
+		asymmetricMatch(received){
+			return received instanceof ApplicationError && received.message.includes(message);
+		},
+		jasmineToString(){
+			return `<ApplicationError ${message}>`;
+		}
+	};
+}
+
+export function delay(ms:number):Promise<void> {
+	return new Promise<void>(resolve => setTimeout(resolve, ms));
+}
+
+export function types<A, B>():[A] extends [B] ? [B] extends [A] ? {
+	areEqual():void;
+} : false : false {
+	return {
+		areEqual(){}
+	} as never;
+}
+
+export const testOptions:ApplicationRunOptions = {
+	throwOnError: true,
+	exitProcessOnHandlerReturn: false,
+};
+
+export function runApp(app:Application, parameters: string[]){
+	const fakePathToApp = path.join(process.cwd(), "test.js");
+	return app.run(["node", fakePathToApp, ...parameters], testOptions);
+}
+
