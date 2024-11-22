df3cf7d41094a49ea4a761cb257ca0be4181717d
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2024-11-22 12:23:44 +0530

    update test app

--- spec/src/test.ts
+++ spec/src/test.ts
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import { Application, arg } from "../../build/index.js";
 
 
@@ -10,11 +9,11 @@ myApp.command("doStuff").description("does stuff").args({
 		option: arg().optional(),
 	}
 }).impl((opts) => {
-	if(opts.namedArgs["sus"]){
+	if(opts.namedArgs.sus){
 		process.stdout.write("sussy ");
 	}
-	if(opts.namedArgs["option"]){
-		console.log(`Option: "${opts.namedArgs["option"]}"`);
+	if(opts.namedArgs.option){
+		console.log(`Option: "${opts.namedArgs.option}"`);
 	}
 	console.log("test command");
 });
@@ -24,26 +23,39 @@ myApp.command("doOtherStuff")
 	.args({
 		namedArgs: {
 			required: arg()
-				.description("You must specify this")
-				.required(),
+				.description("You must specify this"),
 			optional: arg()
 				.description("This is optional")
 				.optional(),
 			defaultVal: arg()
 				.description("this has a default value of SussyAmogus")
-				.default("sussyAmogus"),
+				.default("aa a a a a a"),
 			noValue: arg()
 				.description("this has no value")
 				.valueless(),
+			noValueRequired: arg()
+				.description("this has no value but is required")
+				.valueless()
+				.required(),
 		},
 		aliases: {},
 		positionalArgs: []
 	})
 	.impl((opts, app) => {
-		console.log(`named arg required: ${opts.namedArgs["required"]}`);
-		console.log(`named arg optional: ${opts.namedArgs["optional"]}`);
-		console.log(`named arg defaultVal: ${opts.namedArgs["defaultVal"]}`);
-		console.log(`named arg noValue: ${opts.namedArgs["noValue"]} (in operator: ${"noValue" in opts.namedArgs})`);
+
+
+
+
+
+
+		
+		
+		
+		void opts.namedArgs;
+		console.log(`named arg required: ${opts.namedArgs.required}`);
+		console.log(`named arg optional: ${opts.namedArgs.optional}`);
+		console.log(`named arg defaultVal: ${opts.namedArgs.defaultVal}`);
+		console.log(`named arg noValue: ${opts.namedArgs.noValue} (in operator: ${"noValue" in opts.namedArgs})`);
 		console.log(`positional arg required: ${opts.positionalArgs[0]}`);
 		console.log(`positional arg defaultVal: ${opts.positionalArgs[1]}`);
 		console.log(`positional arg optional: ${opts.positionalArgs[2]}`);
@@ -56,7 +68,7 @@ myApp.command("a").description("tests default values for positional args").args(
 		}
 	]
 }).impl((opts, app) => {
-
+	//
 });
 myApp.command("dashdash").description("tests '--'").args({
 	namedArgs: {
@@ -75,11 +87,11 @@ myApp.category("subcategory1", "Description for subcategory 1", category => {
 			option: arg().optional(),
 		}
 	}).impl((opts) => {
-		if(opts.namedArgs["sus"]){
+		if(opts.namedArgs.sus){
 			process.stdout.write("sussy ");
 		}
-		if(opts.namedArgs["option"]){
-			console.log(`Option: "${opts.namedArgs["option"]}"`);
+		if(opts.namedArgs.option){
+			console.log(`Option: "${opts.namedArgs.option}"`);
 		}
 		console.log("test command");
 	});
